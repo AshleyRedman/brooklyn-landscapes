cb46902312cdd4164613fcbeaa2fbab49bd752d4
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-03-14 22:46:36 +0000

    Vercel doesnt seem to like alias

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import * as C from '@config';
+import * as C from '../config';
 
 const Home: NextPage = () => {
     return (
