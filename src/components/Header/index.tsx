ecf40dbd7ccce6048f9b7d5a51eb8e4aea51189a
Ashley Redman <ashley.redman@jisc.ac.uk>
2022-03-16 00:32:13 +0000

    Small updates

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Menu, Transition } from '@headlessui/react';
+import { MenuIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { FC, Fragment, ReactElement, useState } from 'react';
+import { FC, Fragment, ReactElement } from 'react';
 import logo from '../../../public/logo.jpg';
 import urls from '../../config/urls';
 import Button from '../Button';
 import CustomLink from '../Link';
-
-import { Menu, Transition } from '@headlessui/react';
 
 type NavItemProps = {
     text: string;
@@ -48,14 +47,14 @@ const Nav: FC = (): ReactElement => (
             </ul>
         </nav>
 
-        <Menu as="nav" className="relative block text-left md:hidden">
+        <Menu as="div" className="relative block text-left md:hidden">
             <Menu.Button className="flex items-center rounded-full outline-none focus:ring-2 focus:ring-yellow focus:ring-offset-2">
                 <span className="sr-only">Open menu</span>
                 <MenuIcon className="h-8 w-8 text-green-600" aria-hidden="true" />
             </Menu.Button>
 
             <Transition
-                as="div"
+                as={Fragment}
                 enter="transition ease-out duration-100"
                 enterFrom="transform opacity-0 scale-95"
                 enterTo="transform opacity-100 scale-100"
@@ -64,7 +63,7 @@ const Nav: FC = (): ReactElement => (
                 leaveTo="transform opacity-0 scale-95"
             >
                 <Menu.Items
-                    as="div"
+                    as="nav"
                     className="absolute right-0 mt-2 w-max origin-top-right rounded bg-white shadow-xl outline-none"
                 >
                     <ul className="py-1 px-4 text-right">
@@ -90,7 +89,7 @@ const Nav: FC = (): ReactElement => (
 const Header: FC = (): ReactElement => (
     <header className="border-b-2 border-b-green-400 py-4 px-4">
         <div className="container mx-auto flex items-center justify-between">
-            <CustomLink href={urls.home.path} label="Brooklyn Landscapes" className="w-60">
+            <CustomLink href={urls.home.path} label="Brooklyn Landscapes" className="w-48 md:w-60">
                 <Image src={logo} alt="Brooklyn Landscapes logo" placeholder="blur" />
             </CustomLink>
             <Nav />
